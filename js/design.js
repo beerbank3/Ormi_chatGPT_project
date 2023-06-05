550cfbd430e925674d5feff8f0a90e1798301cda
beerbank3 <yg050140528@gmail.com>
2023-06-05 11:47:35 +0900

    디자인 수정

--- js/design.js
+++ js/design.js
@@ -1,5 +1,6 @@
-var textarea = document.getElementById('area');
+const textarea = document.getElementById('area');
 
 textarea.addEventListener('input', function() {
+  this.style.height = 'auto';
   this.style.height = this.scrollHeight + 'px'; // 내용에 맞게 너비 조정
 });
